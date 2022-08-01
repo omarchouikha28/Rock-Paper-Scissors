974184f4e8b4655d661d65e0e0fedb9bfe9e7f00
Omar Chouikha <81977798+omarchouikha28@users.noreply.github.com>
2022-08-01 13:57:31 +0200

    Final

---
+++ app.js
@@ -0,0 +1,83 @@
+const game = () => {
+    let pScore = 0
+    let cScore = 0
+
+    const startGame = () => {
+        const playButton = document.querySelector('.intro button')
+        const introScreen = document.querySelector('.intro')
+        const match = document.querySelector('.match')
+
+        playButton.addEventListener('click', () => {
+            introScreen.classList.add("fadeOut")
+            match.classList.add("fadeIn")
+        })
+    }
+
+    const playMatch = () => {
+        const options = document.querySelectorAll('.options button')
+        const playerHand = document.querySelector('.player-hand')
+        const computerHand = document.querySelector('.computer-hand')
+        const hands = document.querySelectorAll('.hands img')
+
+        hands.forEach(hand => {
+            hand.addEventListener("animationend", function () {
+                this.style.animation = ""
+            })
+        })
+
+        const computerOptions = ['rock', 'paper', 'scissors']
+        options.forEach(option => {
+            option.addEventListener("click", function () {
+                const computerNumber = Math.floor(Math.random() * 3)
+                const computerChoice = computerOptions[computerNumber]
+
+                playerHand.src = `./assets/rock.png`
+                computerHand.src = `./assets/rock.png`
+
+                setTimeout(() => {
+                    compareHands(this.textContent, computerChoice)
+                    playerHand.src = `./assets/${this.textContent}.png`
+                    computerHand.src = `./assets/${computerChoice}.png`
+                }, 2000)
+
+                playerHand.style.animation = "shakePlayer 2s ease"
+                computerHand.style.animation = "shakeComputer 2s ease"
+            })
+        })
+
+    }
+
+    const compareHands = (playerChoice, computerChoice) => {
+        const winner = document.querySelector('.winner')
+        if (playerChoice === computerChoice) {
+            winner.textContent = "It is a tie!"
+            return;
+        }
+
+        if (playerChoice === 'rock' && computerChoice === 'scissors'
+            || playerChoice === 'scissors' && computerChoice === 'paper'
+            || playerChoice === 'paper' && computerChoice === 'rock') {
+            winner.textContent = "Player wins!"
+            pScore++
+            updateScore()
+            return;
+        } else {
+            winner.textContent = "Computer wins!"
+            cScore++
+            updateScore()
+            return;
+        }
+    }
+
+    const updateScore = () => {
+        const playerScore = document.querySelector('.player-score p')
+        const computerScore = document.querySelector('.computer-score p')
+        playerScore.textContent = pScore
+        computerScore.textContent = cScore
+    }
+
+    startGame()
+    playMatch()
+}
+
+game()
